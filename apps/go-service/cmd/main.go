bf953682f0885e7a9f214135d8c1cd1692865f8a
Dionatan Korb <dmkorb@gmail.com>
2024-05-05 22:43:28 -0300

    feat: go service

--- apps/go-service/cmd/main.go
+++ apps/go-service/cmd/main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	// Add CORS middleware
+	app.Use(cors.New())
+
+	app.Get("/api/data", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello from Go service!",
+		})
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(app.Listen(":" + port))
 }
